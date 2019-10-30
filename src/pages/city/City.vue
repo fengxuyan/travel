7622a1b03f102c70219aaae8f0eca0b8b176dd27
fengxuyan <233992579@qq.com>
2019-10-30 16:14:55 +0800

    city searc

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,19 @@
 <template>
-    <city-header></city-header>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
 </template>
 
 <script>
     import CityHeader from "./components/CityHeader.vue"
+    import CitySearch from "./components/CitySearch.vue"
+
     export default {
         name: "",
-        components:{
-            CityHeader
+        components: {
+            CityHeader,
+            CitySearch
         }
     }
 </script>
---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="search">
+        <input type="text" class="search-input" placeholder="输入城市名或拼音"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CitySearch"
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import "~@s/varibles.styl";
+
+    .search {
+        padding: .1rem;
+        background-color: $bgColor;
+    .search-input {
+        display: block;
+        box-sizing: border-box;
+        padding: 0 .1rem;
+        margin: 0 auto;
+        height: .62rem;
+        width: 96%;
+        line-height: .62rem;
+        border-radius: .1rem;
+        outline: none;
+        border: none;
+        text-align: center;
+        color:#666;
+    }
+
+    }
+</style>
