8118f51c1fcb772c443247df10a5fee4e7b365b1
fengxuyan <233992579@qq.com>
2019-10-30 21:03:05 +0800

    城市列表页面

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
     import CityHeader from "./components/CityHeader.vue"
     import CitySearch from "./components/CitySearch.vue"
+    import CityList from "./components/CityList.vue"
+    import CityAlphabet from "./components/Alphabet.vue"
 
     export default {
         name: "",
         components: {
             CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
         }
     }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">C</li>
+        <li class="item">D</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "CityAlphabet"
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import "~@s/varibles.styl";
+    .list {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: absolute;
+        right:0;
+        top: 1.68rem;
+        bottom: 0;
+        .item {
+            width: .5rem;
+            text-align: center;
+            line-height: .5rem;
+            color: $bgColor;
+            font-size: .33rem;
+
+        }
+    }
+
+</style>
